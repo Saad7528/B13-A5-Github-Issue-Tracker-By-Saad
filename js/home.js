689abe216742e7bd3636d8a193be30e656942e60
saad7528 <saad0174742@gmail.com>
2026-03-07 22:24:34 +0600

    data load complete

--- js/home.js
+++ js/home.js
@@ -18,4 +18,12 @@ function switchBtn(btn){
     });
 }
 
-// const load
+async function loadData() {
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues ")
+    const data = await res.json();
+    console.log(data.data);
+}
+
+
+
+loadData()
