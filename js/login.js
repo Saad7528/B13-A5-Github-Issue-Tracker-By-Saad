0f20780f88c231663ac79976fa0d8f19c277f875
saad7528 <saad0174742@gmail.com>
2026-03-07 21:49:34 +0600

    files organized...

--- js/login.js
+++ js/login.js
@@ -6,12 +6,12 @@ loginBtn.addEventListener('click', function () {
     const passWord = document.getElementById("password")
     // console.log(userName,passWord)
 
-    if(userName.value === "admin" && passWord.value === "1234") {
+    if(userName.value === "admin" && passWord.value === "admin1234") {
         console.log("Yes")
         alert("Log-in Success")
         window.location.assign("./home.html")
     }else{
-        alert("Please input right ")
+        alert("Please Input Correct Username, Password")
         return  
     }
     
